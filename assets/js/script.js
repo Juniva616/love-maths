9777bf242bad204fec1b82a541593a74861cf68a
Juniva616 <juniva616@gmail.com>
2022-08-15 23:13:01 +0000

    Add function t which alculate cs orrect answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,9 +38,22 @@ document.addEventListener("DOMContentLoaded",function() {
  function checkAnswer() {
 
  }
-
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the DOM and returns the correct answer.
+ */
  function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText; 
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+    }
  }
 
  function incrementScore() {
@@ -57,7 +70,7 @@ document.addEventListener("DOMContentLoaded",function() {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent =  "+";
  }
- 
+
  function displaySubtractQuestion() {
     
 }
